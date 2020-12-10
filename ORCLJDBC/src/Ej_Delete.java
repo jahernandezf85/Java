e525a2b86199e8fe193c9ff7c9dd9470d877d64d
Ache lpa <Ache_lpa@yahoo.es>
2020-12-10 10:51:49 +0100

    pruebas conexion oracle 3

--- ORCLJDBC/src/Ej_Delete.java
+++ ORCLJDBC/src/Ej_Delete.java
@@ -1,28 +1,28 @@
-import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+public class Ej_Delete {
 
-public class Ej_Update {
-	public static String cadena(String nombre, BigDecimal salario) {
-		return "UPDATE EMP SET SAL = '" + salario + "' WHERE ENAME = '" + nombre + "'";
+	public static String cadena(String nombre) {
+		return "DELETE FROM EMP WHERE ENAME = '" + nombre + "'";
 	}
-	
 	public static void main(String[] args) {
 		try (Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "scott", "tiger");
 			Statement statement = conn.createStatement();
 			){
 			
-			int row = statement.executeUpdate(cadena("Andrea", new BigDecimal(1600)));
+			int row = statement.executeUpdate(cadena("Andrea"));
 			
-			System.out.println("Numero de filas actualizadas: " + row);
+			System.out.println("Numero de filas eliminadas: " + row);
 			
 		}catch (SQLException e) {
 			System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
+
 	}
+
 }

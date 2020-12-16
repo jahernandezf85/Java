d1f7395cc7148cadd8f0815a22e7fb655c63b680
Ache lpa <Ache_lpa@yahoo.es>
2020-12-16 11:38:34 +0100

    Ej_Select3 corregido

--- JBDC_MYSQL/src/Ej_Select3.java
+++ JBDC_MYSQL/src/Ej_Select3.java
@@ -10,7 +10,8 @@ public class Ej_Select3 {
 	public static void main(String[] args) {
 		String SQL_SELECT = "SELECT PROD.NOMBRE_ESPA PRODUCTO, CAT.NOMBRE_CATE CATEGORIA, PROV.NOMBRE PROVEEDOR"
 				+ " FROM PRODUCTOS PROD INNER JOIN CATEGORIAS CAT ON PROD.CATID = CAT.CATID"
-				+ " INNER JOIN PROVEEDORES PROV ON PROD.PROVID = PROV.PROVID";
+				+ " INNER JOIN PROVEEDORES PROV ON PROD.PROVID = PROV.PROVID"
+				+ " ORDER BY CATEGORIA, PROVEEDOR";
 		
 		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/almacen", "root", "root");
 			Statement statement = conn.createStatement();
@@ -23,7 +24,7 @@ public class Ej_Select3 {
 				String nombre = resultSet.getString("producto");
 				String categoria = resultSet.getString("categoria");
 				String proveedor = resultSet.getString("proveedor");
-				System.out.printf("%-40s %-20s %-20s\n", nombre, categoria, proveedor);
+				System.out.printf("%-20s %-20s %-40s\n", categoria, proveedor, nombre);
 				
 				
 			}

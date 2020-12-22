0bc6d9a2acaf43c4e2a39bb34afd5620bf2532d5
Ache lpa <Ache_lpa@yahoo.es>
2020-12-22 09:49:33 +0100

    Biconsumer

--- j8Consumer/src/BiConsumer/javaBiConsumer1.java
+++ j8Consumer/src/BiConsumer/javaBiConsumer1.java
@@ -1,11 +1,11 @@
 package BiConsumer;
 
+import java.util.TreeMap;
 import java.util.function.BiConsumer;
 
 public class javaBiConsumer1 {
-
-	public static void main(String[] args) {
-		
+	
+	public static void primero() {
 		BiConsumer<Integer, Integer> bic1=new BiConsumer<Integer,Integer>(){
 
 			@Override
@@ -19,6 +19,50 @@ public class javaBiConsumer1 {
 		BiConsumer<Integer, Integer> bic2=(x,y) -> System.out.println(x+y);
 		
 		bic2.accept(5, 3);
+	}
+
+	public static void segundo() {
+		TreeMap<String, Integer> map = new TreeMap<String, Integer>();
+		map.put("Madrid", 10);
+		map.put("Barcelona", 20);
+		map.put("Sevilla", 30);
+		
+		BiConsumer<TreeMap<String,Integer>,Integer> bic1 = new BiConsumer<TreeMap<String,Integer>, Integer>(){
+
+			@Override
+			public void accept(TreeMap<String, Integer> t, Integer u) {
+				for(String clave:t.keySet()) {
+					t.put(clave, t.get(clave)+u);
+				}
+				
+			}
+			
+		};
+		bic1.accept(map, 5);
+		
+		for(String clave:map.keySet()) {
+			System.out.println(clave + " - " + map.get(clave));
+		}
+		
+		System.out.println();
+		System.out.println();
+		/*****************************************************************************************************/
+		BiConsumer<TreeMap<String,Integer>, Integer> bic2=(t,u) ->{
+			for(String clave:t.keySet()) {
+				t.put(clave, t.get(clave)+u);
+			}
+		};
+		
+		bic2.accept(map, 3);
+		for(String clave:map.keySet()) {
+			System.out.println(clave + " - " + map.get(clave));
+		}
+	}
+	public static void main(String[] args) {
+		 
+		//primero();
+		segundo();
+		
 	}
 
 }
